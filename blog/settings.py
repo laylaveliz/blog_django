a14a1547f21864602c87e8a01fb8df3ecef874ff
laylaveliz <lvelizb2@unemi.edu.ec>
2025-10-25 21:26:55 -0500

    Agregado comentario en settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+# Proyecto Django de Layla Veliz PRACTICA 1
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
